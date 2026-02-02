a6244b449aecb83f6327feb16bfcfe7f07332323
simidai <sdaikis2000@gmail.com>
2026-02-02 16:37:03 +0900

    localization

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -19,7 +19,7 @@ export function CatImage({ url }: CatImageProps) {
 
     return (
         <div className={styles.page}>
-            <button onClick={refreshImage} className={styles.button}>他のにゃんこも見る</button>
+            <button onClick={refreshImage} className={styles.button}>One More Cat!</button>
             <div className={styles.frame}>
                 {imageUrl && <img src={imageUrl} className={styles.image}/>}
             </div>
